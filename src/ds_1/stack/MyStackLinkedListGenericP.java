5ad0a0e1b8aaf14037193645f54de3c3b9d6d2ff
kaushgem <kaushcs@gmail.com>
2015-10-01 15:23:32 -0400

    Adding Generic Linked List Impl, according to Princeton non static inner class

---
+++ src/ds_1/stack/MyStackLinkedListGenericP.java
@@ -0,0 +1,61 @@
+/*
+ * Stack implementation using LinkedList with Generic type
+ *  
+ */
+
+package ds_1.stack;
+
+import java.util.EmptyStackException;
+
+/**
+ *
+ * @author kaush
+ * @param <T>
+ */
+public class MyStackLinkedListGenericP<T> {
+    private Node top;
+    
+    private class Node {
+        private T data;
+        private Node next;
+        public Node(T _data) {
+            data = _data;
+        }
+    }
+    
+    public void push(T _data) {
+        Node newN = new Node(_data);
+        newN.next = top;
+        top = newN;
+    }
+    
+    public T pop() {
+        if (top == null) throw new EmptyStackException();
+        T val = top.data;
+        top = top.next;
+        return val;
+    }
+    
+    public T peek() {
+        if (top == null) throw new EmptyStackException();
+        return top.data;
+    }
+    
+    public boolean isEmpty() {
+        return (top == null);
+    }
+    
+    public static void main(String[] args) {
+    	// Diamond Operator
+        MyStackLinkedListGenericP<Integer> stack = new MyStackLinkedListGenericP<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+    }
+}
+    
+
