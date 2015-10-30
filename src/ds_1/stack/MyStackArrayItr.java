8a83295f107a3ad209d9d9cfc3f42c98e173f8bc
kaushgem <kaushcs@gmail.com>
2015-10-29 23:57:07 -0400

    MyStackArrayItr

--- src/ds_1/stack/MyStackArrayItr.java
+++ src/ds_1/stack/MyStackArrayItr.java
@@ -12,66 +12,63 @@ import java.util.Iterator;
  *
  * @author kaush
  */
-public class MyStackArrayItr implements Iterable<String> {
-    
-    private int top;
-    private String[] stack;
+public class MyStackArrayItr implements Iterable<String>{
 
-    public MyStackArrayItr() {
-        top = -1;
-        stack = new String[1];
-    }
-        
-    public void push(String _data) {
-        if(top+1 == stack.length) 
-            increaseStack(2 * stack.length);
-        stack[++top] = _data;
-    }
-    
-    public String pop() {
-        if(top == -1) 
-            throw new EmptyStackException();
-        
-        String val = stack[top];
-        stack[top] = null;
-       
-        if(top > 0 && top == stack.length/4)
-            decreaseStack(stack.length / 2);
-        
-        top--;
-        return val;
-    }
-    
-    public String peek() {
-        if (top == -1)
-            throw new EmptyStackException();
-        return stack[top];
-    }
-    
-    public boolean isEmpty() {
-        return (top == -1);
-    }
-    
-    private void increaseStack(int _capacity) {
-        String[] copy = new String[_capacity];
-        for(int i=0; i<=top; i++)
-            copy[i] = stack[i];
-        stack = copy;
-    }
-    
-    private void decreaseStack(int _capacity) {
-        String[] copy = new String[_capacity];
-        System.arraycopy(stack, 0, copy, 0, top+1); 
-        stack = copy;
-    }
-    
-    @Override
+	private int top;
+	private String[] stack;
+
+	MyStackArrayItr() {
+		top = -1;
+		stack = new String[1];
+	}
+	MyStackArrayItr(int size) {
+		top = -1;
+		stack = new String[size];
+	}
+
+	public void push(String _val) {
+		if(top + 1 == stack.length)
+			modifyStackSize(2 * stack.length);
+		stack[++top] = _val;
+	}
+
+	public String pop() {
+		if(isEmpty())
+			throw new EmptyStackException();
+
+		String val = stack[top];
+		stack[top] = null;
+		top--;
+
+		if(top > 0 && top == stack.length/4)
+			modifyStackSize(stack.length / 2);
+
+		return val;
+	}
+
+	public String peek() {
+		if(isEmpty())
+			throw new EmptyStackException();
+		return stack[top];
+	}
+
+	public boolean isEmpty() {
+		return top < 0;
+	}
+
+	private void modifyStackSize(int _capacity) {
+		String[] copy = new String[_capacity];
+		System.arraycopy(stack, 0, copy, 0, top+1);
+		stack = copy;
+	}
+
+	@Override
 	public Iterator<String> iterator() {
 		return new StackIterator();
 	}
-    
-    private class StackIterator implements Iterator<String> {
-    	int current = top;
+
+	private class StackIterator implements Iterator<String> {
+		int current = top;
 
 		@Override
 		public boolean hasNext() {
@@ -82,34 +79,35 @@ public class MyStackArrayItr implements Iterable<String> {
 		public String next() {
 			return stack[current--];
 		}
-    	
-    }
-    
-    public static void main(String[] args) {
-        MyStackArrayItr s = new MyStackArrayItr();
-        s.push("a");
-        System.out.println("len: "+s.stack.length);
-        s.push("b");
-        System.out.println("len: "+s.stack.length);
-        s.push("c");
-        System.out.println("len: "+s.stack.length);
-        s.push("d");
-        System.out.println("len: "+s.stack.length);
-        s.push("e");
-        
-        for(String str:s)
-        	System.out.println(str);
-        
-        System.out.println("len: "+s.stack.length);
-        System.out.println(s.pop());
-        System.out.println("len: "+s.stack.length);
-        System.out.println(s.pop());
-        System.out.println("len: "+s.stack.length);
-        System.out.println(s.peek());
-        System.out.println("len: "+s.stack.length);
-        System.out.println(s.pop());
-        System.out.println("len: "+s.stack.length);
-    }
+	}
 
-	
+	public static void main(String[] args) {
+		MyStackArrayItr s = new MyStackArrayItr();
+		System.out.println("initial: len: "+ s.top +"/"+s.stack.length);
+		s.push("a");
+		System.out.println("push: len: "+ s.top +"/"+s.stack.length);
+		s.push("b");
+		System.out.println("push: len: "+ s.top +"/"+s.stack.length);
+		s.push("c");
+		System.out.println("push: len: "+ s.top +"/"+s.stack.length);
+		s.push("d");
+		System.out.println("push: len: "+ s.top +"/"+s.stack.length);
+		s.push("e");
+		System.out.println("push: len: "+ s.top +"/"+s.stack.length);
+
+		for(String str:s)
+			System.out.println(str);
+
+		System.out.println("peek: "+s.peek()+" len: "+ s.top +"/"+s.stack.length);
+		System.out.println("peek: "+s.peek()+" len: "+ s.top +"/"+s.stack.length);
+		System.out.println("peek: "+s.peek()+" len: "+ s.top +"/"+s.stack.length);
+		System.out.println("peek: "+s.peek()+" len: "+ s.top +"/"+s.stack.length);
+
+		System.out.println("pop:  "+s.pop()+" len: "+ s.top +"/"+s.stack.length);
+		System.out.println("pop:  "+s.pop()+" len: "+ s.top +"/"+s.stack.length);
+		System.out.println("pop:  "+s.pop()+" len: "+ s.top +"/"+s.stack.length);
+		System.out.println("pop:  "+s.pop()+" len: "+ s.top +"/"+s.stack.length);
+		System.out.println("pop:  "+s.pop()+" len: "+ s.top +"/"+s.stack.length);
+	}
 }
+
